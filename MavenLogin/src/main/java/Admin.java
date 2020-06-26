d8455cb26f3e78c6dd75bfea97201e42661d630c
Elsaeda Shabaj <elsae.shabaj@gmail.com>
2020-06-26 14:23:39 +0200

    maven login

---
+++ MavenLogin/src/main/java/Admin.java
@@ -0,0 +1,30 @@
+
+import javax.swing.JPanel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+class Admin {
+    private JPanel adm;
+    private JPanel admi;
+    public JPanel getAdmin(){
+        JPanel admin=null;
+        return admin;
+    }
+    public void setAdmin(JPanel admin){
+        this.admi=admin;
+    }
+    private JPanel admin;
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ MavenLogin/src/main/java/JLValidation.java
@@ -0,0 +1,24 @@
+
+import java.awt.Color;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+class JLValidation {
+
+    static void setText(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setForegraund(Color red) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
